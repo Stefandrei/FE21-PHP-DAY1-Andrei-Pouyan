03110bc15add3ccb9c591c50c8a21ab6a8a2581b
stefandrei <stefandrei89@outlook.com>
2021-08-09 15:03:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // * exercise 1
+    // $name = "Stefan";
+    // $lastName = "Andrei";   
+    // echo "<h1> $name $lastName </h1>";
+
+
+
+   //* exercise 2 
+// $name = "Stefan";
+// $lastName = "Andrei";
+// $age = "31";
+// $job = "Developer";
+
+// function PrintText(){
+//          global $name, $lastName, $age, $job;
+// 	$fullText = "Hi, my name is ".$name. " " .$lastName. " and i am " .$age. " and i work as a " .$job;
+// 	return $fullText;
+// }
+// echo printText();
+
+//*exercise3
+
+// $arr = array("Mark","John","Georg","Lisa");
+// echo "the third player in the team is ".$arr[2];
+
+    
+
+     $asso_array = array(
+   "cartoon" => "Mickey Maouse",
+   "anime" => "Super Mario",
+   "game" => "packman",
+   );
+echo "cartoon : " .$asso_array["cartoon"]. "<br/>";
+echo "anime : " .$asso_array["anime"]. "<br/>" ;
+echo "game : " .$asso_array["game"];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
+
+
+</body>
+</html>
